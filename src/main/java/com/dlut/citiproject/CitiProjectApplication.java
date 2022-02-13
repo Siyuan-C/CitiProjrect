6c21d8aeeaf8e644053fe1f0da00cf67341b7f63
xzq-123 <3587270081@qq.com>
2022-02-13 20:45:09 +0800

    xzq

--- src/main/java/com/dlut/citiproject/CitiProjectApplication.java
+++ src/main/java/com/dlut/citiproject/CitiProjectApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*
 * Lijiqiao 20:14
+* xzq 20:44
 * */
 @SpringBootApplication
 public class CitiProjectApplication {
