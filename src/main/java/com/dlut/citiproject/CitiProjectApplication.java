901280db7f6319fa855d0fdf3ed3f772ff23892a
Siyuan-C <1195489505@qq.com>
2022-02-13 19:17:08 +0800

    Spring Frame
    2.0

---
+++ src/main/java/com/dlut/citiproject/CitiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dlut.citiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitiProjectApplication.class, args);
+    }
+
+}
