7547e447b22431505bbece04e6d2fcd8901b774a
Lijiqiao <937597275@qq.com>
2022-02-13 20:15:09 +0800

    Lijiqiao 修改了日期20:14

--- src/main/java/com/dlut/citiproject/CitiProjectApplication.java
+++ src/main/java/com/dlut/citiproject/CitiProjectApplication.java
@@ -3,7 +3,7 @@ package com.dlut.citiproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*
-* Lijiqiao 19:40
+* Lijiqiao 20:14
 * */
 @SpringBootApplication
 public class CitiProjectApplication {
